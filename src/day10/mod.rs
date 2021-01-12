d8967cceab79910fdd22089633e7361a66761869
Mikkel Paulson <git@email.mikkel.ca>
2021-01-12 12:11:59 -0500

    day 10 part 2

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -1,11 +1,66 @@
 use std::cmp;
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 use std::fmt;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::iter;
 use std::ops;
 use std::str;
+
+pub fn part1(input: Box<dyn Read>) -> Result<usize, &'static str> {
+    let map = parse(input);
+
+    println!("{:?}", map);
+
+    let best_coord = map.find_station().ok_or("No station coordinate found.")?;
+    println!("Best location is {:?}", best_coord);
+
+    Ok(map.count_asteroids(&best_coord))
+}
+
+pub fn part2(input: Box<dyn Read>) -> Result<usize, &'static str> {
+    let map = parse(input);
+    let station_coord = map.find_station().ok_or("No station coordinate found.")?;
+
+    let mut asteroid_groups = BTreeMap::new();
+    for asteroid in map.asteroids {
+        if asteroid == station_coord {
+            continue;
+        }
+
+        asteroid_groups
+            .entry((station_coord - asteroid).reduce())
+            .or_insert_with(Vec::new)
+            .push(asteroid);
+    }
+
+    asteroid_groups.iter_mut().for_each(|(_, v)| {
+        v.sort_unstable_by(|a, b| {
+            (*a - station_coord)
+                .len()
+                .cmp(&(*b - station_coord).len())
+                .reverse()
+        })
+    });
+
+    let mut i = 0;
+    while !asteroid_groups.is_empty() {
+        for key in asteroid_groups.keys().copied().collect::<Vec<_>>() {
+            i += 1;
+            let asteroid = asteroid_groups.get_mut(&key).map(|v| v.pop()).flatten();
+            println!("{}: {:?}", i, asteroid);
+            if asteroid_groups.get(&key).map_or(false, |v| v.is_empty()) {
+                asteroid_groups.remove(&key);
+            }
+
+            if i == 200 {
+                return Ok(asteroid.unwrap().1 * 100 + asteroid.unwrap().0);
+            }
+        }
+    }
+
+    Err("Ran out of asteroids to destroy.")
+}
 
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 struct Coord(usize, usize);
@@ -38,7 +93,7 @@ impl ops::Sub for Coord {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 struct CoordDiff(isize, isize);
 
 impl CoordDiff {
@@ -46,35 +101,34 @@ impl CoordDiff {
         let gcd = gcd(self.0.abs(), self.1.abs());
         CoordDiff(self.0 / gcd, self.1 / gcd)
     }
-}
 
-pub fn part1(input: Box<dyn Read>) -> Result<usize, &'static str> {
-    let map = parse(input);
-
-    println!("{:?}", map);
-    let mut output = format!("{}", map);
-
-    let mut best_coord = None;
-    let mut best_count = 0;
-
-    for coord in map.asteroids.iter() {
-        let count = map.count_asteroids(coord);
-
-        if count < 10 {
-            let offset = coord.0 * (output.find('\n').unwrap() + 1) + coord.1;
-            output.replace_range(offset..offset + 1, &count.to_string());
-        }
-
-        if count > best_count {
-            best_count = count;
-            best_coord = Some(coord);
+    fn angle(&self) -> f64 {
+        let angle = (self.0 as f64).atan2(self.1 as f64);
+        if angle >= std::f64::consts::FRAC_PI_2 {
+            angle - std::f64::consts::FRAC_PI_2
+        } else {
+            angle + std::f64::consts::PI + std::f64::consts::FRAC_PI_2
         }
     }
 
-    println!("{}", output);
-    println!("Best location is {:?}", best_coord);
+    fn len(&self) -> usize {
+        (self.0.abs() + self.1.abs()) as usize
+    }
+}
 
-    Ok(best_count)
+impl cmp::Ord for CoordDiff {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        match self.angle().partial_cmp(&other.angle()) {
+            Some(cmp::Ordering::Equal) | None => self.len().cmp(&other.len()),
+            Some(o) => o,
+        }
+    }
+}
+
+impl cmp::PartialOrd for CoordDiff {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 fn gcd(a: isize, b: isize) -> isize {
@@ -91,6 +145,21 @@ struct Map {
 }
 
 impl Map {
+    fn find_station(&self) -> Option<Coord> {
+        let mut best_coord = None;
+        let mut best_count = 0;
+
+        for coord in self.asteroids.iter() {
+            let count = self.count_asteroids(coord);
+            if count > best_count {
+                best_count = count;
+                best_coord = Some(*coord);
+            }
+        }
+
+        best_coord
+    }
+
     fn count_asteroids(&self, station_coord: &Coord) -> usize {
         let mut count = 0;
         for asteroid_coord in self.asteroids.iter() {
