396e43802c117a9ac1bdc78d3a06f5f75a6eded7
Mikkel Paulson <git@email.mikkel.ca>
2021-04-12 19:43:10 -0400

    day 16 part 2: solution!
    Only works for offsets in the second half of the problem space, but
    coincidentally (not coincidentally), all of the examples use offsets in
    the second half of the problem space!

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -1,5 +1,4 @@
-use std::collections::HashMap;
-use std::mem;
+use std::iter;
 use std::ops::Range;
 
 pub fn part1(input: &str) -> Result<u64, String> {
@@ -7,11 +6,17 @@ pub fn part1(input: &str) -> Result<u64, String> {
 }
 
 pub fn part2(input: &str) -> Result<u64, String> {
-    Err("Not implemented".to_string())
+    let offset = cycles_slice(&parse(input)?, 0..7, 1, 0) as usize;
+    Ok(cycles_slice(
+        &parse(input)?,
+        offset..offset + 8,
+        10_000,
+        100,
+    ))
 }
 
 fn cycles_slice(
-    input: &Vec<i16>,
+    input: &Vec<u8>,
     range: Range<usize>,
     repetitions: usize,
     cycle_count: usize,
@@ -24,27 +29,36 @@ fn cycles_slice(
         .sum()
 }
 
-fn cycles(input: &Vec<i16>, start_pos: usize, repetitions: usize, cycle_count: usize) -> Vec<i16> {
+fn cycles(input: &Vec<u8>, start_pos: usize, repetitions: usize, cycle_count: usize) -> Vec<u8> {
     let mut digits = (0..repetitions)
         .flat_map(|_| input.iter())
         .copied()
         .collect();
 
     for i in 0..cycle_count {
+        println!("Cycle {}", i);
         cycle(&mut digits, start_pos);
-        println!("Cycle {}: {:?}", i, digits);
     }
 
     digits
 }
 
-fn cycle(input: &mut Vec<i16>, start_pos: usize) {
-    let result = (start_pos..input.len()).map(|i| digit(&input, i)).collect();
-    mem::replace(input, result);
+fn cycle(input: &mut Vec<u8>, start_pos: usize) {
+    *input = if start_pos > input.len() / 2 {
+        let mut result: Vec<u8> = iter::repeat(0).take(input.len()).collect();
+        let mut acc: u64 = 0;
+        for i in (start_pos..input.len()).rev() {
+            acc = acc + input[i] as u64;
+            result[i] = (acc % 10) as u8;
+        }
+        result
+    } else {
+        (start_pos..input.len()).map(|i| digit(&input, i)).collect()
+    };
 }
 
-fn digit(input: &[i16], calc_pos: usize) -> i16 {
-    let mut acc: i16 = 0;
+fn digit(input: &[u8], calc_pos: usize) -> u8 {
+    let mut acc: i64 = 0;
     let mut i = 0;
     let repetitions = calc_pos + 1;
     let half_step = repetitions * 2;
@@ -53,29 +67,29 @@ fn digit(input: &[i16], calc_pos: usize) -> i16 {
     while i + calc_pos < input.len() {
         let mut start = i + calc_pos;
         let mut end = (start + calc_pos).min(input.len() - 1);
-        let sum: i16 = input[start..=end].iter().sum();
+        let sum: i64 = input[start..=end].iter().map(|i| *i as i64).sum();
         acc = acc + sum;
 
         start += half_step;
         if start < input.len() {
             end = (end + half_step).min(input.len() - 1);
-            let sum: i16 = input[start..=end].iter().sum();
+            let sum: i64 = input[start..=end].iter().map(|i| *i as i64).sum();
             acc -= sum;
         }
 
         i += step;
     }
 
-    acc.abs() % 10
+    (acc.abs() % 10) as u8
 }
 
-fn parse(input: &str) -> Result<Vec<i16>, String> {
+fn parse(input: &str) -> Result<Vec<u8>, String> {
     input
         .trim()
         .chars()
         .map(|c| {
             c.to_digit(10)
-                .map(|c| c as i16)
+                .map(|c| c as u8)
                 .ok_or_else(|| format!("Invalid digit: {}", c))
         })
         .collect::<Result<_, _>>()
@@ -105,7 +119,6 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn part2_examples() {
         assert_eq!(Ok(84462026), part2("03036732577212944063491565474664"));
         assert_eq!(Ok(78725270), part2("02935109699940807407585447034323"));
@@ -115,6 +128,6 @@ mod test {
     #[test]
     #[ignore]
     fn part2_solution() {
-        assert_eq!(Ok(0), part2(include_str!("input.txt")));
+        assert_eq!(Ok(85600369), part2(include_str!("input.txt")));
     }
 }
