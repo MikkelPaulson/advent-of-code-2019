290f79af79e8b16025e3aa6e89b02adea3391bf1
Mikkel Paulson <git@email.mikkel.ca>
2021-04-12 19:26:24 -0400

    day 16 part 1: optimize

--- src/day16/mod.rs
+++ src/day16/mod.rs
@@ -3,15 +3,20 @@ use std::mem;
 use std::ops::Range;
 
 pub fn part1(input: &str) -> Result<u64, String> {
-    let output = cycles(&parse(input)?, 1, 100);
-    Ok(slice_digits(&output, 0..8))
+    Ok(cycles_slice(&parse(input)?, 0..8, 1, 100))
 }
 
 pub fn part2(input: &str) -> Result<u64, String> {
     Err("Not implemented".to_string())
 }
 
-fn slice_digits(digits: &Vec<i16>, range: Range<usize>) -> u64 {
+fn cycles_slice(
+    input: &Vec<i16>,
+    range: Range<usize>,
+    repetitions: usize,
+    cycle_count: usize,
+) -> u64 {
+    let digits = cycles(input, range.start, repetitions, cycle_count);
     let range_end = range.end;
 
     range
@@ -19,22 +24,22 @@ fn slice_digits(digits: &Vec<i16>, range: Range<usize>) -> u64 {
         .sum()
 }
 
-fn cycles(input: &Vec<i16>, repetitions: usize, cycles: usize) -> Vec<i16> {
+fn cycles(input: &Vec<i16>, start_pos: usize, repetitions: usize, cycle_count: usize) -> Vec<i16> {
     let mut digits = (0..repetitions)
         .flat_map(|_| input.iter())
         .copied()
         .collect();
 
-    for i in 0..cycles {
-        cycle(&mut digits);
+    for i in 0..cycle_count {
+        cycle(&mut digits, start_pos);
         println!("Cycle {}: {:?}", i, digits);
     }
 
     digits
 }
 
-fn cycle(input: &mut Vec<i16>) {
-    let result = (0..input.len()).map(|i| digit(&input, i)).collect();
+fn cycle(input: &mut Vec<i16>, start_pos: usize) {
+    let result = (start_pos..input.len()).map(|i| digit(&input, i)).collect();
     mem::replace(input, result);
 }
 
@@ -78,16 +83,16 @@ fn parse(input: &str) -> Result<Vec<i16>, String> {
 
 #[cfg(test)]
 mod test {
-    use super::{cycles, part1, part2, slice_digits};
+    use super::{cycles_slice, part1, part2};
 
     #[test]
     fn part1_examples() {
         let digits = vec![1, 2, 3, 4, 5, 6, 7, 8];
 
-        assert_eq!(48226158, slice_digits(&cycles(&digits, 1, 1), 0..8));
-        assert_eq!(34040438, slice_digits(&cycles(&digits, 1, 2), 0..8));
-        assert_eq!(03415518, slice_digits(&cycles(&digits, 1, 3), 0..8));
-        assert_eq!(01029498, slice_digits(&cycles(&digits, 1, 4), 0..8));
+        assert_eq!(48226158, cycles_slice(&digits, 0..8, 1, 1));
+        assert_eq!(34040438, cycles_slice(&digits, 0..8, 1, 2));
+        assert_eq!(03415518, cycles_slice(&digits, 0..8, 1, 3));
+        assert_eq!(01029498, cycles_slice(&digits, 0..8, 1, 4));
 
         assert_eq!(Ok(24176176), part1("80871224585914546619083218645595"));
         assert_eq!(Ok(73745418), part1("19617804207202209144916044189917"));
@@ -95,7 +100,6 @@ mod test {
     }
 
     #[test]
-    #[ignore]
     fn part1_solution() {
         assert_eq!(Ok(96136976), part1(include_str!("input.txt")));
     }
